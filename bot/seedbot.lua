bf5c9a1669565ab64f011c4f6b4a71cdf9dd6b0b
mrfoxirani <mr.fox@usa.com>
2015-12-09 18:48:26 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {176229065},--Sudo users
+    sudo_users = {160553465},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
